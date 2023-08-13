954a120f4e2db60972944e381daec26cb339c92e
alghazu <alghazu@gmail.com>
2023-08-13 02:27:24 +0200

    removed all js of contact form)

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,42 +1,40 @@
-$(function() {
+// $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // You can retain the form field value retrieval here
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
+//     $("input,textarea").jqBootstrapValidation({
+//         preventSubmit: true,
+//         submitError: function($form, event, errors) {
+//             // additional error messages or events
+//         },
+//         submitSuccess: function($form, event) {
+//             event.preventDefault(); // prevent default submit behaviour
+//             // You can retain the form field value retrieval here
+//             var name = $("input#name").val();
+//             var email = $("input#email").val();
+//             var phone = $("input#phone").val();
+//             var message = $("textarea#message").val();
+//             var firstName = name; // For Success/Failure Message
+//             // Check for white space in name for Success/Fail message
+//             if (firstName.indexOf(' ') >= 0) {
+//                 firstName = name.split(' ').slice(0, -1).join(' ');
+//             }
 
-            // You can remove the $.ajax() code block entirely
+//             // Clear the form fields
+//             $('#contactForm').trigger("reset");
+//         },
+//         filter: function() {
+//             return $(this).is(":visible");
+//         },
+//     });
 
-            // Clear the form fields
-            $('#contactForm').trigger("reset");
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
-
-    $("a[data-toggle=\"tab\"]").click(function(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    });
-});
+//     $("a[data-toggle=\"tab\"]").click(function(e) {
+//         e.preventDefault();
+//         $(this).tab("show");
+//     });
+// });
 
 
 
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
-});
+// /*When clicking on Full hide fail/success boxes */
+// $('#name').focus(function() {
+//     $('#success').html('');
+// });
